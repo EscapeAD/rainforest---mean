7d0bbd6be0396172f4a36795fbab5e79e8047f9d
Adam Tse <mr.adam.tse@gmail.com>
2017-01-15 16:26:43 -0500

    added product schema/mongodb connect

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,21 @@
-const express = require('express'),
-      router  = express.Router();
+const express     = require('express'),
+      mongoose    = require('mongoose'),
+      Product     = require('../models/product'),
+      router      = express.Router();
+
+mongoose.connect('mongodb://localhost/rainforest')
+
+//
+router.get('/products', (req, res)=>{
+  Product.find({}, (err, products)=>{
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
+      render.json(products);
+    }
+  })
+})
 
 
 module.exports = router;
