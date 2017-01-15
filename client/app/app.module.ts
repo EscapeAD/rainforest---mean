ca4cdb6016c15bbc6214f55b2ef0429918be55d9
Adam Tse <mr.adam.tse@gmail.com>
2017-01-15 17:16:43 -0500

    client index/bootstrap/basics

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NavComponent } from './components/navbar/nav.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, ProductsComponent, NavComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ client/app/components/navbar/nav.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'navmenu',
+  templateUrl: 'nav.component.html'
+})
+export class NavComponent {}
