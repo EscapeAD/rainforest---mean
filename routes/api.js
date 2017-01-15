afc4b15612b4eeee11893184cd72992da73ea2a7
Adam Tse <mr.adam.tse@gmail.com>
2017-01-15 16:36:13 -0500

    added products routes

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ const express     = require('express'),
 
 mongoose.connect('mongodb://localhost/rainforest')
 
-//
+//ALL
 router.get('/products', (req, res)=>{
   Product.find({}, (err, products)=>{
     if(err){
@@ -13,6 +13,56 @@ router.get('/products', (req, res)=>{
       res.send(err);
     } else {
       render.json(products);
+    }
+  })
+})
+
+//Show
+router.get('/products/:id', (req, res)=>{
+  Product.findById(req.params.id, (err, product)=>{
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
+      render.json(product);
+    }
+  })
+})
+
+//create
+router.post('/products', (req, res)=>{
+  let newProduct = req.body;
+  Product.create(newProduct, (err, products)=>{
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
+      render.json(products);
+    }
+  })
+})
+
+//edit
+router.put('/products/:id', (req, res)=>{
+  let update = req.body;
+  Product.findByIdAndUpdate(req.params.id, update, (err, product)=>{
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
+      render.json(product);
+    }
+  })
+})
+
+//destroy
+router.delete('/products/:id', (req, res)=>{
+  Product.findByIdAndRemove(req.params.id, (err, product)=>{
+    if(err){
+      console.log(err);
+      res.send(err);
+    } else {
+      render.json(product);
     }
   })
 })
